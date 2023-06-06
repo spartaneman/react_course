0b3c7b1c4f39cf938d42df0d2351013d43e5761e
emanRuiz <Emanuel.Ruiz@student.csulb.edu>
2023-06-05 18:17:18 -0700

    adding components for user input

---
+++ src/components/newExpenses/ExpenseForm.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './ExpenseForm.css';
+
+const ExpenseForm = () =>{
+
+
+    return(
+        <form>
+            <div className='new-expense__controls'>
+                <div className='new-expense__control'>
+                    <label>Title</label>
+                    <input type='text' />
+                </div>
+                <div className='new-expense__control'>
+                    <label>Amount</label>
+                    <input type='number' />
+                </div>
+                <div className='new-expense__control'>
+                    <label>Date</label>
+                    <input type='date' />
+                </div>
+            </div>
+        </form>
+    );
+    
+}
+
+export default ExpenseForm;
